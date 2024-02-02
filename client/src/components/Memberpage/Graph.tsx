ab62cb71caeefe91124909fc1e88347136224ece
Nang K Tu <ntu2391007@gmail.com>
2024-02-02 02:02:00 +0000

    Final

--- client/src/components/Memberpage/Graph.tsx
+++ client/src/components/Memberpage/Graph.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import WeightChart from './WeightChart';
 import { Chart, registerables } from 'chart.js';
 Chart.register(...registerables);
@@ -10,50 +10,46 @@ interface WeightData {
 
 const YourApp: React.FC = () => {
   const [weightData, setWeightData] = useState<WeightData[]>([]);
-  const [loading, setLoading] = useState(true);
+  const [showChart, setShowChart] = useState<boolean>(false);
 
-  useEffect(() => {
-    const fetchWeightData = async () => {
-      try {
-        const token = sessionStorage.getItem('token');
-        if (!token) {
-          console.error('Token not found in sessionStorage.');
-          return;
-        }
-
-        const response = await fetch('/api/user-weights', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        if (response.ok) {
-          const data: WeightData[] = await response.json();
-          setWeightData(data);
-        } else {
-          console.error('Error fetching weight data:', response.statusText);
-        }
-      } catch (error) {
-        console.error('Error fetching weight data:', error);
-      } finally {
-        setLoading(false);
+  const fetchDataAndToggleChart = async () => {
+    try {
+      const token = sessionStorage.getItem('token');
+      if (!token) {
+        console.error('Token not found in sessionStorage.');
+        return;
       }
-    };
 
-    fetchWeightData();
-  }, []);
+      const response = await fetch('/api/user-weights', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.ok) {
+        const data: WeightData[] = await response.json();
+        setWeightData(data);
+        setShowChart((prevShowChart) => !prevShowChart); // Toggle the value
+      } else {
+        console.error('Error fetching weight data:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error fetching weight data:', error);
+    }
+  };
 
   const last7DaysData = weightData.slice(-7);
 
   return (
     <div className="justify-content-center d-flex">
-      {loading ? (
-        <p>Loading weight data...</p>
-      ) : (
-        <WeightChart data={last7DaysData} />
-      )}
+      <div>
+        <button onClick={fetchDataAndToggleChart}>
+          {showChart ? 'Hide Chart' : 'Show Chart'}
+        </button>
+      </div>
+      <div>{showChart && <WeightChart data={last7DaysData} />}</div>
     </div>
   );
 };
